210ec2682e609dc3d925c07f74610bdd64e3aa77
Parinya Khemthong <parinya2541@hotmai.com>
2025-11-28 01:00:25 +0700

    install antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// Ant Design reset styles (v5+)
+import 'antd/dist/reset.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
