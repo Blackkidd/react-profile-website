94da5e2b7e0ba0947ab7d3b30ef51dc8b5ce2c65
Parinya Khemthong <parinya2541@hotmai.com>
2025-12-09 11:10:39 +0700

    test commmit with lazy git

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { Button, Card, Typography, Space } from 'antd'
 import './App.css'
 
 
 function App() {
   return (
     <div className="p-8 min-h-screen bg-gray-50">
-      {/* <Card
-        title="Test Ant Design Card"
-        className="max-w-md mx-auto shadow-lg"
-      >
-        <p className="text-xl font-bold text-blue-600">Parinya Khemthong</p>
-        <p className="text-gray-700">Web Developer</p>
-        <p className="text-gray-600 italic">Programmer</p>
-      </Card> */}
+      <h1 className="text-3xl font-bold underline text-center text-blue-600">
+        Hello, World!
+      </h1>
     </div>
   )
 }
